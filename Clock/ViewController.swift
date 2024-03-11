a399f5e7e602cd19f7eed15990bd773d3a73c860
KT <thun.kristian@gmail.com>
2024-03-11 14:42:00 +0100

    Start rework to chessClock

--- Clock/ViewController.swift
+++ Clock/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    let formatter = DateFormatter()
+    var timer : Timer?
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        formatter.timeStyle = .medium
+    }
+    
+
+    
+    @IBAction func showClock(_ sender: Any) {
+        updateTime()
+        startClock()
+    }
+    
+    
+    
+    func startClock(){
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: updateTime(timer:))
+        
+    }
+    
+    func updateTime(timer: Timer? = nil){
+        let date = Date()
+        let dateString = formatter.string(from: date)
+        counterLabel.text = dateString
+    }
+    
+    func stopClock(){
+        timer?.invalidate()
     }
 
+    deinit {
+        stopClock()
+    }
 
 }
 
