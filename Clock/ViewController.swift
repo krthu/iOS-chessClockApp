bc1d99a980a7fb76c4aa49036203ff08d771f377
KT <thun.kristian@gmail.com>
2024-03-12 13:28:01 +0100

    Works kinda

--- Clock/ViewController.swift
+++ Clock/ViewController.swift
@@ -14,43 +14,69 @@ class ViewController: UIViewController {
     var timer : Timer?
     
     var gameTime = 5
+
     
     var timeLeftPlayer1 = 5 * 60
     var timeLeftPlayer2 = 5 * 60
+    var isPlayer1Active = false
     
-    @IBOutlet weak var counterLabel: UILabel!
+
+    
+    @IBOutlet weak var player2Button: UIButton!
+    
+    @IBOutlet weak var player1Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         formatter.timeStyle = .medium
-        counterLabel.text = getTimeString(toConvert: timeLeftPlayer1)
+        player2Button.setTitle(getTimeString(toConvert: timeLeftPlayer2), for: .normal)
+        player1Button.setTitle(getTimeString(toConvert: timeLeftPlayer1), for: .normal)
         
     }
     
-
-    
-    @IBAction func showClock(_ sender: Any) {
-        //updateTime()
+    @IBAction func showClock(_ sender: UIButton) {
+        
+        if sender.tag == 1 {
+            isPlayer1Active = false
+            
+        } else if sender.tag == 2{
+            isPlayer1Active = true
+            
+        }
+        player1Button.isEnabled = isPlayer1Active
+        player2Button.isEnabled = !isPlayer1Active
         startClock()
+       
+        
     }
     
     func startClock(){
         if timer == nil {
-            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: updatePlayerTime(timer:))
-            
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block:updatePlayerTime(timer:))
         }
-    }
-    
-    func updateTime(timer: Timer? = nil){
-        let date = Date()
-        let dateString = formatter.string(from: date)
-        counterLabel.text = dateString
+        
     }
     
     func updatePlayerTime(timer: Timer? = nil){
-        timeLeftPlayer1 -= 1
-        let timeFormated = getTimeString(toConvert: timeLeftPlayer1)
-        counterLabel.text = String(timeFormated)
+        
+        if isPlayer1Active{
+            timeLeftPlayer1 -= 1
+            let timeFormated = getTimeString(toConvert: timeLeftPlayer1)
+            player1Button.setTitle(timeFormated, for: .normal)
+   
+            
+        } else{
+            timeLeftPlayer2 -= 1
+            let timeFormated = getTimeString(toConvert: timeLeftPlayer2)
+            player2Button.setTitle(timeFormated, for: .normal)
+        }
+        if timeLeftPlayer1 == 0 || timeLeftPlayer2 == 0{
+            timer?.invalidate()
+        }
+
+        
+        
+        
     }
     
     func stopClock(){
